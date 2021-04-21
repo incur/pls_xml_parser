c24583b547ab734b3a912dc690160fc24efd7fca
incur <wbraschos@gmail.com>
2021-04-21 11:40:04 +0200

    fixed bad code copy

--- app/tools/manipulation.py
+++ app/tools/manipulation.py
@@ -1,13 +1,65 @@
-import time
+import re
 
 
-def timeit(method):
-    def timed(*args, **kw):
-        ts = time.time()
-        result = method(*args, **kw)
-        te = time.time()
-        tz = te-ts
-        if tz >= 0.05:
-            print(f'{tz:.2f} sec, {method.__name__}')
-        return result
-    return timed
+class fragile(object):
+    class Break(Exception):
+        """Break out of the with statement"""
+
+    def __init__(self, value):
+        self.value = value
+
+    def __enter__(self):
+        return self.value.__enter__()
+
+    def __exit__(self, etype, value, traceback):
+        error = self.value.__exit__(etype, value, traceback)
+        if etype == self.Break:
+            return True
+        return error
+
+
+def check_lines(file):
+    lines = search_lines(file, ['<Cr id', 'Rezeptoperation läuft'])
+    charge = filter_string(lines[0], "name='", "' hdl=")
+    product = filter_string(lines[0], "productname='", "' productcode=")
+    recipe = filter_string(lines[0], "recipeprocedurename='", "' recipeprocedureversion=")
+    start = filter_string(lines[0], "actstart='", "' actend=")
+    ende = filter_string(lines[0], "actend='", "' startmodetype")
+    area = filter_string(lines[1], "area='", "' />")
+
+<< << << < HEAD
+
+anlage = area + '_#_' + product
+
+return [charge, anlage, area, product, recipe, start, ende]
+== == == =
+
+return [charge, area, product, recipe, start, ende]
+>> >> >> > 83
+d59c853e43ff740a5578ddd361c0c882e86d3f
+
+
+def search_lines(file, substrings):
+    found = [None] * len(substrings)
+    with fragile(open(file, encoding='utf-16')) as f:
+        for line in f:
+            for i, sub in enumerate(substrings):
+                if sub in line:
+                    found[i] = line
+            if all(v is not None for v in found):
+                return found
+                raise fragile.Break
+
+
+def filter_string(s, start, end):
+    return re.search('%s(.*)%s' % (start, end), s).group(1)
+
+
+def check_file_charge(filename, df):
+    splitted_filename = filename.split('_')
+    file_charge = splitted_filename[2] + '_' + splitted_filename[3]
+
+    if file_charge in df['charge'].unique():
+        return True
+    else:
+        return False
