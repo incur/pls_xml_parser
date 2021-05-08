3cb5e860ee38b320f74fb7d254f0569bd42aae0c
incur <wbraschos@gmail.com>
2021-05-08 23:46:16 +0200

    now with plots

--- run.py
+++ run.py
@@ -1,18 +1,10 @@
 import sys
-from app import main, test
+from app import main
 
 
 def run():
     main.main()
 
 
-def run_test():
-    test.main()
-
-
 if __name__ == '__main__':
-    arg = sys.argv
-    if str(arg[1]) == 'test':
-        run_test()
-    else:
-        run()
+    run()
